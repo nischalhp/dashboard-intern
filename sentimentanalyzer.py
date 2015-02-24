f0c7480920081ad6c2329c4b633d235ed7cca1e6
Imadnagina <imad@redmart.com>
2015-02-24 10:09:51 +0530

    Update sentimentanalyzer.py

--- sentimentanalyzer.py
+++ sentimentanalyzer.py
@@ -2,35 +2,36 @@ import pymongo
 from pymongo import MongoClient
 from textblob import TextBlob
 from textblob.sentiments import NaiveBayesAnalyzer
+import time
+import re
+import datetime
 
-
-client = pymongo.MongoClient('mongodb://localhost:27017')
+client = pymongo.MongoClient('mongodb://localhost:27017/tweetDB2')
 db = client['tweetDB2']
 RedMartc = db['@RedMartcom']
 
 
 
 def get_sentiments(data):
+
     
+    text = data['tweet_text']
     
+    blob = TextBlob(text,analyzer=NaiveBayesAnalyzer())
     
-    blob = TextBlob(data['tweet_text'],analyzer=NaiveBayesAnalyzer())
     
     print  data['tweet_ID']
     
-    RedMartc.update({"tweet_ID":{"$exists":True}},{"$set":{"classify":blob.sentiment.classification,"pos":blob.sentiment.p_pos,"neg":blob.sentiment.p_neg}},multi=True)
+    print  blob.sentiment.classification
+    
+    RedMartc.update({"tweet_ID":data['tweet_ID']},{"$set":{"classify":blob.sentiment.classification,"pos":blob.sentiment.p_pos,"neg":blob.sentiment.p_neg}},multi=True)
+
 
 def visited():
-    
-    tweets = RedMartc.find({ "classify": { "$exists":False}})
-    
-    if RedMartc.find( { "classify": { "$exists":False}} ):
-        print 'NO MORE TWEETS TO ANALYZE'
-    
+    tweets = RedMartc.find({"classify": { "$exists":False}})
     for RedMart in tweets:
         get_sentiments(RedMart)
         time.sleep(5)
 
       
 visited()
-
