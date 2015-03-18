a8823667badbccb7aa08669098397e80a926f8a3
Nischal HP <nischal.hp@gmail.com>
2015-03-18 16:54:11 +0530

    Removing new line and unwanted spaces

--- dashboard/app.py
+++ dashboard/app.py
@@ -12,11 +12,20 @@ def index():
     tweets = mongo.db.RedMartcom.find({"classify": { "$exists":True}}).sort([("Created_at", -1)])
     countcl = mongo.db.countclassify.find({"cneg": {  "$exists":True}})
 
+    tweetsOut = []
+    for tweet in tweets:
+    	tweetText = tweet["tweet_text"]
+    	tweetText = tweetText.encode('utf-8').replace('\n','').replace('\r','')
+    	tweet["tweet_text"] = tweetText.decode('utf-8')
+    	tweetsOut.append(tweet)
+
+
     for RedMart in tweets,countcl:
 
-        return render_template('index.html',tweets=tweets,countcl=countcl)
+        return render_template('index.html',tweets=tweetsOut,countcl=countcl)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
+    #app.run(debug=True)
 
 
